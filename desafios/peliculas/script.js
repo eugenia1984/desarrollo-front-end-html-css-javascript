986d70aa26a94e65f1cf22a5a0d7651cc756273b
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-10-31 20:22:28 -0300

    -agrego un index para hacer una web con links y cambio carpetas de orden

--- desafios/peliculas/script.js
+++ desafios/peliculas/script.js
@@ -1,5 +1,27 @@
 const d = document
 
+// Slider
+const castList = d.querySelector('.cast-list')
+const prevBtn = d.getElementById('prevBtn')
+const nextBtn = d.getElementById('nextBtn')
+const itemWidth = d.querySelector('.cast-list li').offsetWidth
+let currentPosition = 0
+
+nextBtn.addEventListener('click', () => {
+  if (currentPosition > -itemWidth * (castList.childElementCount - 1)) {
+    currentPosition -= itemWidth
+    castList.style.transform = `translateX(${currentPosition}px)`
+  }
+})
+
+prevBtn.addEventListener('click', () => {
+  if (currentPosition < 0) {
+    currentPosition += itemWidth
+    castList.style.transform = `translateX(${currentPosition}px)`
+  }
+})
+
+// Modal de See all
 const addDisplayBlock = () => {
   d.getElementById('myModal').style.display = 'block'
 }
